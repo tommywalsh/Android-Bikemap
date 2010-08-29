54f33b040362e94a01e4351f39ecfad1413c01c0
tommywalsh <ymmothslaw@gmail.com>
2010-08-28 22:26:37 -0400

    Show an indicatior for current position Always keep the current position centered A.B.S.: Always Be Scrolling

--- src/com/github/tommywalsh/map/BikeMapActivity.java
+++ src/com/github/tommywalsh/map/BikeMapActivity.java
@@ -4,30 +4,85 @@ import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapView;
 import com.google.android.maps.MapController;
 import com.google.android.maps.GeoPoint;
-import android.os.Bundle;
+import com.google.android.maps.Overlay;
 
-public class BikeMapActivity extends MapActivity
+import android.location.LocationManager;
+import android.location.LocationListener;
+import android.location.Location;
+import android.location.Criteria;
+import android.content.Context;
+import android.os.Bundle;
+import android.graphics.Canvas;
+import android.graphics.Point;
+import android.graphics.Paint;
+
+public class BikeMapActivity extends MapActivity implements LocationListener
 {
+    public void onLocationChanged(Location loc) {
+	if (loc != null) {
+	    m_location = new GeoPoint((int)(loc.getLatitude() * 1E6),
+			      (int)(loc.getLongitude() * 1E6));
+	    m_controller.animateTo(m_location);
+	}
+    }
+    public void onProviderDisabled(String provider) {
+    }
+    public void onProviderEnabled(String provider) {
+    }
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+    }
+
+
+    public class LocationIndicator extends Overlay {
+	public boolean draw(Canvas canvas, MapView mapView, boolean shadow, long when) {
+	    if (m_location != null) {
+		Point screenCoords = new Point();
+		super.draw(canvas, mapView, shadow, when);
+		Paint paint = new Paint();
+		
+		mapView.getProjection().toPixels(m_location, screenCoords);
+		paint.setStrokeWidth(1);
+		if (shadow) {
+		    paint.setARGB(255,0,0,255);
+		    paint.setStyle(Paint.Style.FILL_AND_STROKE);
+		} else {		    
+		    paint.setARGB(255,0,255,0);
+		    paint.setStyle(Paint.Style.STROKE);
+		}
+		canvas.drawCircle(screenCoords.x, screenCoords.y, 5, paint);
+
+	    }
+	    return true;
+	}
+    }
+
+
     @Override
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+	m_locManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+	m_locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 50, this);
 	
 	MapView mapview = (MapView) findViewById(R.id.mapview);
 	mapview.setBuiltInZoomControls(true);
+	mapview.getOverlays().add(new LocationIndicator());
+	
 	m_controller = mapview.getController();
 
-	m_controller.setCenter(new GeoPoint(42378778, -71095667));
 	m_controller.setZoom(16); // 16 seems best for biking speeds
+	m_controller.setCenter(new GeoPoint(42378778, -71095667)); // Union Square
     }
 
     @Override protected boolean isRouteDisplayed() {
 	return false;
     }
 
+
     MapController m_controller;
-    //    LocationManager m_locmanager;
-    
+    LocationManager m_locManager;
+    GeoPoint m_location;
 }
 
 
