c19cebfe507bf7d14eecd28ea9d578008836506a
tommywalsh <ymmothslaw@gmail.com>
2010-08-29 19:30:43 -0400

    Add ability to show a pre-planned route.

--- src/com/github/tommywalsh/map/BikeMapActivity.java
+++ src/com/github/tommywalsh/map/BikeMapActivity.java
@@ -16,10 +16,16 @@ import android.graphics.Canvas;
 import android.graphics.Point;
 import android.graphics.Paint;
 
+import android.util.Log;
+
+import java.util.TreeSet;
+
 public class BikeMapActivity extends MapActivity implements LocationListener
 {
+    Location m_oldLoc;
     public void onLocationChanged(Location loc) {
-	if (loc != null) {
+	if (loc != null && !loc.equals(m_oldLoc)) {
+	    m_oldLoc = loc;
 	    m_location = new GeoPoint((int)(loc.getLatitude() * 1E6),
 			      (int)(loc.getLongitude() * 1E6));
 	    m_controller.animateTo(m_location);
@@ -34,12 +40,14 @@ public class BikeMapActivity extends MapActivity implements LocationListener
 
 
     public class LocationIndicator extends Overlay {
+	
 	public boolean draw(Canvas canvas, MapView mapView, boolean shadow, long when) {
+	    super.draw(canvas, mapView, shadow, when);
+	    
 	    if (m_location != null) {
 		Point screenCoords = new Point();
-		super.draw(canvas, mapView, shadow, when);
-		Paint paint = new Paint();
-		
+
+		Paint paint = new Paint();		
 		mapView.getProjection().toPixels(m_location, screenCoords);
 		paint.setStrokeWidth(1);
 		if (shadow) {
@@ -51,23 +59,23 @@ public class BikeMapActivity extends MapActivity implements LocationListener
 		}
 		canvas.drawCircle(screenCoords.x, screenCoords.y, 5, paint);
 
-	    }
+		}
 	    return true;
 	}
     }
 
 
-    @Override
-	public void onCreate(Bundle savedInstanceState) {
+    @Override public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-
+	
 	m_locManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 	m_locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 50, this);
 	
 	MapView mapview = (MapView) findViewById(R.id.mapview);
 	mapview.setBuiltInZoomControls(true);
 	mapview.getOverlays().add(new LocationIndicator());
+	mapview.getOverlays().add(m_route);
 	
 	m_controller = mapview.getController();
 
@@ -79,10 +87,10 @@ public class BikeMapActivity extends MapActivity implements LocationListener
 	return false;
     }
 
-
     MapController m_controller;
     LocationManager m_locManager;
     GeoPoint m_location;
+    RouteOverlay m_route = new RouteOverlay();
 }
 
 
---
+++ src/com/github/tommywalsh/map/RouteOverlay.java
@@ -0,0 +1,122 @@
+package com.github.tommywalsh.map;
+
+import java.util.TreeSet;
+import java.util.TreeMap;
+import java.util.SortedMap;
+import java.util.Vector;
+import java.util.Scanner;
+import java.io.File;
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.Overlay;
+import com.google.android.maps.MapView;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.graphics.Path;
+import android.graphics.Color;
+
+import android.util.Log;
+
+public class RouteOverlay extends Overlay
+{
+
+    public void draw(Canvas canvas, MapView mv, boolean shadow) {
+
+	// Try calulating the path once, then using offset each draw for speed?
+
+	super.draw(canvas, mv, shadow);
+
+	if (shadow) {
+
+	    GeoPoint ctr = mv.getMapCenter();
+	    int latSpan = mv.getLatitudeSpan();
+	    int lat1 = ctr.getLatitudeE6() - latSpan/2;
+	    int lat2 = lat1 + latSpan;
+	    int lngSpan = mv.getLongitudeSpan();
+	    int lng1 = ctr.getLongitudeE6() - lngSpan/2;
+	    int lng2 = lng1 + lngSpan;
+
+	    if (m_path == null || !ctr.equals(m_lastCenter)) {
+
+		// We need to calculate a new path
+		m_path = new Path();
+		m_lastCenter = ctr;
+		
+		boolean drewLastPoint = false;
+		for (int i = 0; i < m_route.size(); i++) {
+		    GeoPoint gp = m_route.elementAt(i);
+		    int lat = gp.getLatitudeE6();
+		    int lng = gp.getLongitudeE6();
+		    Point thisPoint = new Point();
+		    Point otherPoint = new Point();
+		    
+		    
+		    if (lat >= lat1 && lat <= lat2 && lng >= lng1 && lng <= lng2) {
+			// this point is in view.  We will draw from/to here
+			mv.getProjection().toPixels(gp, thisPoint);
+			if (drewLastPoint) {
+			    // keep drawing
+			    Log.d("path", "toHere");
+			    m_path.lineTo(thisPoint.x, thisPoint.y); 
+			} else if (i != 0) {
+			    // draw from the last off-screen point
+			    Log.d("path", "toHereFromOffscreen");
+			    mv.getProjection().toPixels(m_route.elementAt(i-1), otherPoint);
+			    m_path.moveTo(otherPoint.x, otherPoint.y);
+			    m_path.lineTo(thisPoint.x, thisPoint.y); 
+			} else {
+			    // start here
+			    Log.d("path", "startHere");
+			    m_path.moveTo(thisPoint.x, thisPoint.y);
+			}
+			drewLastPoint = true;
+		    } else {
+			// This point is not in view.  Draw to here if we're the first off-screen point
+			if (drewLastPoint) {
+			    Log.d("path", "toOffscreen");
+			    mv.getProjection().toPixels(gp, thisPoint);
+			    m_path.lineTo(thisPoint.x, thisPoint.y);
+			}
+			drewLastPoint = false;
+		    }
+		}
+	    }
+
+	    if (!m_path.isEmpty()) {
+		Paint mPaint = new Paint();
+		mPaint.setDither(true);
+		mPaint.setColor(Color.RED);
+		mPaint.setStyle(Paint.Style.STROKE);
+		mPaint.setStrokeJoin(Paint.Join.ROUND);
+		mPaint.setStrokeCap(Paint.Cap.ROUND);
+		mPaint.setStrokeWidth(2);
+		
+		canvas.drawPath(m_path, mPaint);
+	    }
+	}
+    }
+
+    public RouteOverlay() {
+	try {
+	    Scanner scanner = new Scanner(new File("/sdcard/mapdata/coords"));
+	    int idx = 0;
+	    while (scanner.hasNextDouble()) {
+		int lng = (int)(scanner.nextDouble()*1E6);
+		if (scanner.hasNextDouble()) {
+		    int lat = (int)(scanner.nextDouble()*1E6);
+		    if (scanner.hasNextDouble()) {
+			scanner.nextDouble();
+		    }		
+		    GeoPoint thisPoint = new GeoPoint(lat,lng);
+		    m_route.add(thisPoint);
+		    Integer i = new Integer(lat);
+		}
+	    }
+	} catch (java.io.FileNotFoundException e) {
+	}
+    }
+
+    Path m_path;
+    GeoPoint m_lastCenter;
+    Vector<GeoPoint> m_route = new Vector<GeoPoint>();
+}
