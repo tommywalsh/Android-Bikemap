cc39bec9cb4795485d31211b9610d2d552ccb391
tommywalsh <ymmothslaw@gmail.com>
2010-08-28 21:26:57 -0400

    Disallow scrolling by swiping Zoom in on Union Square by default

--- src/com/github/tommywalsh/map/BikeMapActivity.java
+++ src/com/github/tommywalsh/map/BikeMapActivity.java
@@ -2,6 +2,8 @@ package com.github.tommywalsh.map;
 
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapView;
+import com.google.android.maps.MapController;
+import com.google.android.maps.GeoPoint;
 import android.os.Bundle;
 
 public class BikeMapActivity extends MapActivity
@@ -13,11 +15,19 @@ public class BikeMapActivity extends MapActivity
 	
 	MapView mapview = (MapView) findViewById(R.id.mapview);
 	mapview.setBuiltInZoomControls(true);
+	m_controller = mapview.getController();
+
+	m_controller.setCenter(new GeoPoint(42378778, -71095667));
+	m_controller.setZoom(16); // 16 seems best for biking speeds
     }
 
     @Override protected boolean isRouteDisplayed() {
 	return false;
     }
+
+    MapController m_controller;
+    //    LocationManager m_locmanager;
+    
 }
 
 
