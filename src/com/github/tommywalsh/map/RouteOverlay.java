01c436ca18de890c89a57d78f980d98f81be1bd6
tommywalsh <ymmothslaw@gmail.com>
2010-09-03 14:11:52 -0400

    Refactor: Break path calculation into its own function in anticipation of future changes

--- src/com/github/tommywalsh/map/RouteOverlay.java
+++ src/com/github/tommywalsh/map/RouteOverlay.java
@@ -16,10 +16,18 @@ import android.graphics.Path;
 import android.graphics.Color;
 import android.os.Handler;
 
+import android.util.Log;
 
 // This class handles drawing of a path on a map.
+
 // The path is stored as KML file, and displayed as a semi-translucent overlay 
 // over the map.
+
+// It tries to be smart about recalculating screen positions for the points.
+// That is, if you don't move, it shouldn't need to do any calculations.
+// If you only move "a little", it shouldn't need to do much.
+
+
 public class RouteOverlay extends Overlay
 {
 
@@ -29,6 +37,87 @@ public class RouteOverlay extends Overlay
     public void loadDataSet(String filename) {
 	startIOProcess(filename);	
     }
+
+    // The data about the route is stored as latitude/longitude pairs
+    // This function's job is to convert these into on-screen pixel positions,
+    // and store them in a 'Path' that the canvas can draw on screen.
+    // It caches its calculations and tries not to do more work than necessary
+    public void calculateNewPathIfNeeded(MapView mv)
+    {
+	// Do we need to calculate a new path?  Only if we've moved since the last call
+	// Note, it might be more efficient to give a little "slop" to this, so that 
+	// a small amount of motion is allowed before recalulating the whole path.  In that
+	// case we'd simply translate() the path based on the number of pixels moved since last
+	// time.  But, we can't allow moves that are "too big" since the map projection change
+	// does not imply necessarily a pixel-for-pixel translation over larger distances.
+	GeoPoint ctr = mv.getMapCenter();
+	
+	if (m_path == null || !ctr.equals(m_lastCenter))  {
+	    // We need to calculate a new path
+	    m_path = new Path();
+	    
+	    // Figure screen span
+	    int latSpan = mv.getLatitudeSpan();
+	    int lat1 = ctr.getLatitudeE6() - latSpan/2;
+	    int lat2 = lat1 + latSpan;
+	    int lngSpan = mv.getLongitudeSpan();
+	    int lng1 = ctr.getLongitudeE6() - lngSpan/2;
+	    int lng2 = lng1 + lngSpan;
+	    
+	    // cache center for next time
+	    m_lastCenter = ctr;
+	    
+	    
+	    // Each tip may have one or more legs.  Try to draw them all
+	    for (TripLeg leg : m_legs) {
+		
+		// We can be more efficient here by short-circuiting and not considering
+		// legs that aren't even partially on-screen
+		
+		// Iterate over each point in the leg, and figure out if we need to add it
+		// to the drawn path.  The analogy here is a pen.  
+		boolean drewLastPoint = false;
+		for (int i = 0; i < leg.size(); i++) {
+		    GeoPoint gp = leg.elementAt(i);
+		    int lat = gp.getLatitudeE6();
+		    int lng = gp.getLongitudeE6();
+		    Point thisPoint = new Point();
+		    Point otherPoint = new Point();
+		    
+		    
+		    if (lat >= lat1 && lat <= lat2 && lng >= lng1 && lng <= lng2) {
+			// this point is in view.  We will need to do some drawing
+			mv.getProjection().toPixels(gp, thisPoint);
+			if (drewLastPoint) {
+			    // Last point was also on screen... 
+			    // The pen is on the paper.  Draw to the current point.
+			    m_path.lineTo(thisPoint.x, thisPoint.y); 
+			} else if (i != 0) {
+			    // The last point was off-screen.  Pick up the pen and put it there,
+			    // then draw to the current point.
+			    mv.getProjection().toPixels(leg.elementAt(i-1), otherPoint);
+			    m_path.moveTo(otherPoint.x, otherPoint.y);
+			    m_path.lineTo(thisPoint.x, thisPoint.y); 
+			} else {
+			    // This is the first point in the leg.
+			    // Pick up the pen and position it at this point.
+			    m_path.moveTo(thisPoint.x, thisPoint.y);
+			}
+			drewLastPoint = true;
+		    } else {
+			// This point is not on screen...
+			if (drewLastPoint) {
+			    // ... but the last point was.  Draw to this off-screen point to finish the path
+			    mv.getProjection().toPixels(gp, thisPoint);
+			    m_path.lineTo(thisPoint.x, thisPoint.y);
+			}
+			drewLastPoint = false;
+		    }
+		}
+	    }
+	}
+    }
+
 
     // Called periodically by the Android system when it wants us to redraw the path
     public void draw(Canvas canvas, MapView mv, boolean shadow) {
@@ -44,99 +133,25 @@ public class RouteOverlay extends Overlay
 	    // Since we draw only one thing, we only need to draw during one of these
 	    // Pick shadow, in case we ever add anything later that we want to appear
 	    // on top of the path.
-	    if (shadow) {
-		
-		// Do we need to calculate a new path?  Only if we've moved since the last call
-		// Note, it might be more efficient to give a little "slop" to this, so that 
-		// a small amount of motion is allowed before recalulating the whole path.  In that
-		// case we'd simply translate() the path based on the number of pixels moved since last
-		// time.  But, we can't allow moves that are "too big" since the map projection change
-		// does not imply necessarily a pixel-for-pixel translation over larger distances.
-		GeoPoint ctr = mv.getMapCenter();
-		
-		if (m_path == null || !ctr.equals(m_lastCenter))  {
-		    // We need to calculate a new path
-		    m_path = new Path();
-		    
-		    // Figure screen span
-		    int latSpan = mv.getLatitudeSpan();
-		    int lat1 = ctr.getLatitudeE6() - latSpan/2;
-		    int lat2 = lat1 + latSpan;
-		    int lngSpan = mv.getLongitudeSpan();
-		    int lng1 = ctr.getLongitudeE6() - lngSpan/2;
-		    int lng2 = lng1 + lngSpan;
-		    
-		    // cache center for next time
-		    m_lastCenter = ctr;
+	    if (shadow) {		
+		calculateNewPathIfNeeded(mv);
 
-
-		    // Each tip may have one or more legs.  Try to draw them all
-		    for (TripLeg leg : m_legs) {
-			
-			// We can be more efficient here by short-circuiting and not considering
-			// legs that aren't even partially on-screen
-
-			// Iterate over each point in the leg, and figure out if we need to add it
-			// to the drawn path.  The analogy here is a pen.  
-			boolean drewLastPoint = false;
-			for (int i = 0; i < leg.size(); i++) {
-			    GeoPoint gp = leg.elementAt(i);
-			    int lat = gp.getLatitudeE6();
-			    int lng = gp.getLongitudeE6();
-			    Point thisPoint = new Point();
-			    Point otherPoint = new Point();
-			    
-			    
-			    if (lat >= lat1 && lat <= lat2 && lng >= lng1 && lng <= lng2) {
-				// this point is in view.  We will need to do some drawing
-				mv.getProjection().toPixels(gp, thisPoint);
-				if (drewLastPoint) {
-				    // Last point was also on screen... 
-				    // The pen is on the paper.  Draw to the current point.
-				    m_path.lineTo(thisPoint.x, thisPoint.y); 
-				} else if (i != 0) {
-				    // The last point was off-screen.  Pick up the pen and put it there,
-				    // then draw to the current point.
-				    mv.getProjection().toPixels(leg.elementAt(i-1), otherPoint);
-				    m_path.moveTo(otherPoint.x, otherPoint.y);
-				    m_path.lineTo(thisPoint.x, thisPoint.y); 
-				} else {
-				    // This is the first point in the leg.
-				    // Pick up the pen and position it at this point.
-				    m_path.moveTo(thisPoint.x, thisPoint.y);
-				}
-				drewLastPoint = true;
-			    } else {
-				// This point is not on screen...
-				if (drewLastPoint) {
-				    // ... but the last point was.  Draw to this off-screen point to finish the path
-				    mv.getProjection().toPixels(gp, thisPoint);
-				    m_path.lineTo(thisPoint.x, thisPoint.y);
-				}
-				drewLastPoint = false;
-			    }
-			}
-		    }
+		// Now, we've got a fully-calculated path.  Put it on the screen (if it's not empty)
+		if (!m_path.isEmpty()) {
+		    Paint paint = new Paint();
+		    paint.setDither(true);
+		    paint.setARGB(126,0,126,255);
+		    paint.setStyle(Paint.Style.STROKE);
+		    paint.setStrokeJoin(Paint.Join.ROUND);
+		    paint.setStrokeCap(Paint.Cap.ROUND);
+		    paint.setStrokeWidth(5);		
+		    canvas.drawPath(m_path, paint);
 		}
 	    }
-
-
-
-	    // Now, we've got a fully-calculated path.  Put it on the screen (if it's not empty)
-	    if (!m_path.isEmpty()) {
-		Paint paint = new Paint();
-		paint.setDither(true);
-		paint.setARGB(126,0,126,255);
-		paint.setStyle(Paint.Style.STROKE);
-		paint.setStrokeJoin(Paint.Join.ROUND);
-		paint.setStrokeCap(Paint.Cap.ROUND);
-		paint.setStrokeWidth(5);		
-		canvas.drawPath(m_path, paint);
-	    }
-
+	    
 	} else {
 	    // We get here if the data hasn't been loaded.  Just show a message.
-
+	    
 	    Paint paint = new Paint();
 	    if (shadow) {
 		// Draw a black rectangular background...
@@ -149,7 +164,7 @@ public class RouteOverlay extends Overlay
 		paint.setStyle(Paint.Style.STROKE);
 		canvas.drawText("Loading Route Data...", 10, 20, paint);
 	    }
-
+	    
 	    // And cache the map so we can force a redraw when we get the data loaded.
 	    m_mapView = mv;
 	}
@@ -161,13 +176,11 @@ public class RouteOverlay extends Overlay
     GeoPoint m_lastCenter;    // Cache the last center point so we don't bother redrawing the path unless it changes
     Path m_path;              // Also cache the path for potential reuse next draw cycle
 
-    
-
 
     // We do our IO operations in another thread.  It can take in a while to read in a whole bike trip's
     // worth of position data.  Don't hang the UI thread during this time! 
 
-// If we draw before we load our data, cache the mapview so we can force it to redraw    
+    // If we draw before we load our data, cache the mapview so we can force it to redraw    
     MapView m_mapView = null;        
 
     // ONLY to be used on the UI thread!
